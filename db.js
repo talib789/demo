dff5a378814dc52f316b7564b2e9f8c6ad0c76df
talib789 <taalibullah786@gmail.com>
2022-08-12 23:52:42 +0530

    yoo!

--- db.js
+++ db.js
@@ -2,12 +2,13 @@ const items = require("./data/items");
 const tags = require("./data/tags");
 const brands = require("./data/brands");
 const itemTypes = require("./data/itemTypes");
-
+const sugar = require("./data/sugar");
 module.exports = {
   items,
   tags,
   brands,
-  itemTypes
+  itemTypes,
+  sugar
 }
 
 
